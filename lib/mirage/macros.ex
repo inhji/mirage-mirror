bdbc5279ff21b5af3ec1f050fadda730d2eab97b
Inhji <johnnie@posteo.de>
2021-11-28 17:28:35 +0100

    feat: add autocomplete lib

---
+++ lib/mirage/macros.ex
@@ -0,0 +1,11 @@
+defmodule Mirage.Macros do
+  defmacro contains(content, search_term) do
+    quote do
+      fragment(
+        "LOWER(?) %> LOWER(?)",
+        unquote(content),
+        unquote(search_term)
+      )
+    end
+  end
+end
