f31e99ef3200a7d6afbdb8d2b3f64d3f6b028a33
Inhji <johnnie@posteo.de>
2021-11-28 14:43:45 +0100

    feat: add rel alias

--- mix.exs
+++ mix.exs
@@ -66,7 +66,8 @@ defmodule Mirage.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.deploy": ["esbuild default --minify", "phx.digest"]
+      "assets.deploy": ["esbuild default --minify", "phx.digest"],
+      rel: ["git_ops.release --yes"]
     ]
   end
 end
