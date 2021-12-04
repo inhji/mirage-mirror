f19aecafd1c16107139b9f430014f33a7cc75338
Inhji <johnnie@posteo.de>
2021-12-04 09:57:52 +0100

    feat: add references proper

--- lib/mirage/references.ex
+++ lib/mirage/references.ex
@@ -1,29 +1,55 @@
 defmodule Mirage.References do
-  @hashid_reference_regex ~r/\[\[(?<id>[\d\w-]+)(?:\|(?<title>[\w\d\s']+))?\]\]/
+  @moduledoc """
+  Finds and replaces references to entities in a string
+  """
 
+  alias MirageWeb.Router.Helpers, as: Routes
+
+  @hashid_reference_regex ~r/\[\[(?:(?<type>list|tag):)?(?<id>[\d\w-]+)(?:\|(?<title>[\w\d\s']+))?\]\]/
+
+  @doc """
+  Returns a list of references in the string
+  """
   def get_references(string) do
     @hashid_reference_regex
     |> Regex.scan(string, capture: :all)
     |> Enum.map(&map_to_tuple/1)
   end
 
-  def replace_references(string, opts \\ [admin: false]) do
-    path =
-      if opts[:admin] do
-        "/admin/notes"
-      else
-        "/notes"
-      end
-
+  @doc """
+  Finds and replaces references with the matching url
+  """
+  def replace_references(string) do
     string
     |> get_references()
-    |> Enum.reduce(string, fn {placeholder, slug, title}, s ->
-      String.replace(s, placeholder, get_link(slug, title, path))
+    |> Enum.reduce(string, fn {placeholder, type, slug, title}, s ->
+      String.replace(s, placeholder, get_link(type, slug, title))
     end)
   end
 
-  defp get_link(slug, title, path), do: "[#{title}](#{path}/#{slug})"
+  defp get_link(type, slug, title) do
+    path =
+      case type do
+        "tag" ->
+          "/nothing"
 
-  defp map_to_tuple([placeholder, note_slug]), do: {placeholder, note_slug, note_slug}
-  defp map_to_tuple([placeholder, note_slug, title]), do: {placeholder, note_slug, title}
+        "list" ->
+          "/nothing"
+
+        _ ->
+          Routes.admin_note_path(MirageWeb.Endpoint, :show, slug)
+      end
+
+    do_get_link(title, path)
+  end
+
+  defp do_get_link(title, path) do
+    "[#{title}](#{path})"
+  end
+
+  defp map_to_tuple([placeholder, type, note_slug]),
+    do: {placeholder, type, note_slug, note_slug}
+
+  defp map_to_tuple([placeholder, type, note_slug, title]),
+    do: {placeholder, type, note_slug, title}
 end
--- test/mirage/references_test.exs
+++ test/mirage/references_test.exs
@@ -3,6 +3,7 @@ defmodule Mirage.ReferencesTest do
 
   alias Mirage.Notes
   alias Mirage.Notes.Note
+  import Mirage.References, only: [get_references: 1, replace_references: 1]
 
   setup do
     {:ok, %Note{} = note1} =
@@ -15,20 +16,37 @@ defmodule Mirage.ReferencesTest do
   end
 
   describe "get_references" do
+    test "get_references/1 finds references" do
+      assert [{"[[test]]", "", "test", "test"}] ==
+               get_references("this is a [[test]]")
+
+      assert [{"[[test|Some Title]]", "", "test", "Some Title"}] ==
+               get_references("this is a [[test|Some Title]]")
+
+      assert [{"[[tag:test]]", "tag", "test", "test"}] ==
+               get_references("this is a [[tag:test]]")
+
+      assert [{"[[list:test]]", "list", "test", "test"}] ==
+               get_references("this is a [[list:test]]")
+
+      assert [{"[[list:test|Some Title]]", "list", "test", "Some Title"}] ==
+               get_references("this is a [[list:test|Some Title]]")
+    end
+
     test "get_references/1 finds self references", %{note1: note1} do
       assert {:ok, %Note{} = note1} =
                Notes.update_note(note1, %{content: "this is a link to [[some-note]]"})
 
-      assert [{"[[some-note]]", "some-note", "some-note"}] =
-               Mirage.References.get_references(note1.content)
+      assert [{"[[some-note]]", "", "some-note", "some-note"}] ==
+               get_references(note1.content)
     end
 
     test "get_references/1 finds self references with title", %{note1: note1} do
       assert {:ok, %Note{} = note1} =
                Notes.update_note(note1, %{content: "this is a link to [[some-note|Some Note]]"})
 
-      assert [{"[[some-note|Some Note]]", "some-note", "Some Note"}] =
-               Mirage.References.get_references(note1.content)
+      assert [{"[[some-note|Some Note]]", "", "some-note", "Some Note"}] ==
+               get_references(note1.content)
     end
 
     test "get_references/1 finds references to other notes", %{note1: note1} do
@@ -37,8 +55,8 @@ defmodule Mirage.ReferencesTest do
                  content: "this is a link to [[some-other-note|Some other note]]"
                })
 
-      assert [{"[[some-other-note|Some other note]]", "some-other-note", "Some other note"}] =
-               Mirage.References.get_references(note1.content)
+      assert [{"[[some-other-note|Some other note]]", "", "some-other-note", "Some other note"}] ==
+               get_references(note1.content)
     end
   end
 
@@ -47,24 +65,24 @@ defmodule Mirage.ReferencesTest do
       assert {:ok, %Note{} = note1} =
                Notes.update_note(note1, %{content: "this is a link to [[some-note]]"})
 
-      assert "this is a link to [some-note](/notes/some-note)" =
-               Mirage.References.replace_references(note1.content)
+      assert "this is a link to [some-note](/admin/notes/some-note)" ==
+               replace_references(note1.content)
     end
 
     test "replace_references/2 replaces a self-reference with title", %{note1: note1} do
       assert {:ok, %Note{} = note1} =
                Notes.update_note(note1, %{content: "this is a link to [[some-note|A Title]]"})
 
-      assert "this is a link to [A Title](/notes/some-note)" =
-               Mirage.References.replace_references(note1.content)
+      assert "this is a link to [A Title](/admin/notes/some-note)" ==
+               replace_references(note1.content)
     end
 
     test "replace_references/2 replaces references to other notes", %{note1: note1} do
       assert {:ok, %Note{} = note1} =
                Notes.update_note(note1, %{content: "this is a link to [[some-other-note]]"})
 
-      assert "this is a link to [some-other-note](/notes/some-other-note)" =
-               Mirage.References.replace_references(note1.content)
+      assert "this is a link to [some-other-note](/admin/notes/some-other-note)" ==
+               replace_references(note1.content)
     end
 
     test "replace_references/2 replaces references to other notes with title", %{note1: note1} do
@@ -73,8 +91,8 @@ defmodule Mirage.ReferencesTest do
                  content: "this is a link to [[some-other-note|Another title]]"
                })
 
-      assert "this is a link to [Another title](/notes/some-other-note)" =
-               Mirage.References.replace_references(note1.content)
+      assert "this is a link to [Another title](/admin/notes/some-other-note)" ==
+               replace_references(note1.content)
     end
   end
 end
