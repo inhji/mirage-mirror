4f2fe0da5c177ca25efc177cba8e6598f7fbe0ca
Inhji <johnnie@posteo.de>
2021-12-26 23:53:17 +0100

    feat: categorize themes

--- lib/mirage_web/controllers/page_controller.ex
+++ lib/mirage_web/controllers/page_controller.ex
@@ -2,7 +2,13 @@ defmodule MirageWeb.PageController do
   use MirageWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html", page_title: "Home")
+    notes = Mirage.Notes.list_published_notes()
+
+    render(conn, "index.html", page_title: "Home", notes: notes)
+  end
+
+  def about(conn, _params) do
+    render(conn, "about.html", page_title: "About Me")
   end
 
   def theme(conn, _params) do
