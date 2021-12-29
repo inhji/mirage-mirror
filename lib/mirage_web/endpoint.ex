d8b58a8019f0f33b1a4800ccf66e5413d18c33d5
Inhji <johnnie@posteo.de>
2021-12-29 21:04:49 +0100

    feat: add manifest.json to Endpoint config

--- lib/mirage_web/endpoint.ex
+++ lib/mirage_web/endpoint.ex
@@ -20,7 +20,7 @@ defmodule MirageWeb.Endpoint do
     at: "/",
     from: :mirage,
     gzip: false,
-    only: ~w(assets docs fonts images favicon.ico robots.txt)
+    only: ~w(assets docs fonts images manifest.json favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
