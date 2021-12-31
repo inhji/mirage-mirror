7eab2e332d9a1adcaba94d1b722d686e9fe0c402
Inhji <johnnie@posteo.de>
2021-12-31 11:57:07 +0100

    feat: create color palette from tailwind colors

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   mode: 'jit',
   // https://tailwindcss.com/docs/configuration#important
@@ -8,14 +10,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
-        base: '#EDEAEA',
-        low: '#DDDDDD',
-        med: '#808790',
-        high: '#393B3F',
-        accent1: '#ff78a9',
-        accent2: '#00aefe'
-      }
+      colors: ({ theme }) => ({
+        base: colors.gray['100'],
+        low: colors.gray['300'],
+        med: colors.gray['500'],
+        high: colors.gray['800'],
+        accent1: colors.pink['500'],
+        accent2: colors.sky['500']
+      })
     }
   },
   plugins: [
