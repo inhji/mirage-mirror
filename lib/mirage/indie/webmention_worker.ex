a372e3a7607c322246e5dddeb7ab05a1dbc81701
Inhji <johnnie@posteo.de>
2022-01-04 20:51:42 +0100

    fix: add error handling to

--- lib/mirage/indie/webmention_worker.ex
+++ lib/mirage/indie/webmention_worker.ex
@@ -9,15 +9,27 @@ defmodule Mirage.Indie.WebmentionWorker do
 
   @impl Oban.Worker
   def perform(%Oban.Job{args: %{"url" => url} = _args}) do
-    case Webmentions.send_webmentions(url) do
-      {:ok, responses} ->
-        Enum.each(responses, fn response ->
-          meta = Map.from_struct(response)
-          Mirage.Logger.info("Webmention for url [#{url}] sent successfully!", meta)
-        end)
+    try do
+      case Webmentions.send_webmentions(url) do
+        {:ok, responses} ->
+          Enum.each(responses, fn response ->
+            meta = Map.from_struct(response)
+            Mirage.Logger.info("Webmention for url [#{url}] sent successfully!", meta)
+          end)
 
-      {:error, reason} ->
-        Mirage.Logger.error("Sending webmention for url [#{url}] failed!", %{reason: reason})
+        {:error, reason} ->
+          Mirage.Logger.error("Sending webmention for url [#{url}] failed!", %{reason: reason})
+      end
+    rescue
+      error ->
+        Logger.warn("Error when sending webmentions!")
+        Logger.error(inspect(__STACKTRACE__))
+        Logger.error(inspect(error))
+
+        Mirage.Logger.error("Sending webmention for url [#{url}] failed!", %{
+          error: error,
+          stacktrace: __STACKTRACE__
+        })
     end
 
     :ok
