b50cc30c61881de607cb41b5c407bc2a9cd18a2d
Inhji <johnnie@posteo.de>
2022-01-04 14:13:11 +0100

    feat: move hcard to homepage

--- lib/mirage_web/controllers/page_controller.ex
+++ lib/mirage_web/controllers/page_controller.ex
@@ -2,12 +2,12 @@ defmodule MirageWeb.PageController do
   use MirageWeb, :controller
 
   def index(conn, _params) do
+    user = Mirage.Accounts.get_user()
     notes = Mirage.Notes.list_published_notes()
-    render(conn, "index.html", page_title: "Home", notes: notes)
+    render(conn, "index.html", page_title: "Home", notes: notes, user: user)
   end
 
   def about(conn, _params) do
-    user = Mirage.Accounts.get_user()
-    render(conn, "about.html", page_title: "About", user: user)
+    render(conn, "about.html", page_title: "About")
   end
 end
