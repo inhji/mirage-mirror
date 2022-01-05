551bb1c51f93a386ea9288f328333332483c0fb6
Inhji <johnnie@posteo.de>
2022-01-05 09:27:12 +0100

    feat: add test for about and fix index test

--- test/mirage_web/controllers/page_controller_test.exs
+++ test/mirage_web/controllers/page_controller_test.exs
@@ -1,8 +1,14 @@
 defmodule MirageWeb.PageControllerTest do
   use MirageWeb.ConnCase
 
+  setup :register_and_log_in_user
+
   test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
+    conn = get(conn, Routes.page_path(conn, :index))
     assert html_response(conn, 200)
+  end
+
+  test "GET /about", %{conn: conn} do
+    conn = get(conn, Routes.page_path(conn, :about))
   end
 end
