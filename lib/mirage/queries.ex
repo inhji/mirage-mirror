d67a426006a809704d7c87379f318de21a60be27
Inhji <johnnie@posteo.de>
2022-01-06 09:54:11 +0100

    feat: add queries module for common internal queries

---
+++ lib/mirage/queries.ex
@@ -0,0 +1,48 @@
+defmodule Mirage.Queries do
+  import Ecto.Query, warn: false
+
+  def order_by_query(query, opts) do
+    case opts["order_by"] do
+      "published_at_desc" ->
+        query |> order_by([n], desc: n.published_at)
+
+      "published_at_asc" ->
+        query |> order_by([n], asc: n.published_at)
+
+      "inserted_at_desc" ->
+        query |> order_by([n], desc: n.inserted_at)
+
+      "inserted_at_asc" ->
+        query |> order_by([n], asc: n.inserted_at)
+
+      _ ->
+        query
+    end
+  end
+
+  def search_query(query, opts) do
+    case opts["search_query"] do
+      "" ->
+        query
+
+      search_query ->
+        str = "%#{search_query}%"
+        query |> where([n], ilike(n.title, ^str))
+    end
+  end
+
+  def published_query(query, opts) do
+    case opts["show_published"] do
+      "published" -> query |> where([n], not is_nil(n.published_at))
+      "unpublished" -> query |> where([n], is_nil(n.published_at))
+      _ -> query
+    end
+  end
+
+  def list_query(query, opts) do
+    case opts["show_list"] do
+      "all" -> query
+      list_id -> query |> where([n], n.list_id == ^list_id)
+    end
+  end
+end
