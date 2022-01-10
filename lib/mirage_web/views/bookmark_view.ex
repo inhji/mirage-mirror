674b77099308f58100605d5318a5b5af13567532
Inhji <johnnie@posteo.de>
2022-01-10 23:01:16 +0100

    feat: use view helpers

---
+++ lib/mirage_web/views/bookmark_view.ex
@@ -0,0 +1,3 @@
+defmodule MirageWeb.BookmarkView do
+  use MirageWeb, :view
+end
--- lib/mirage_web/views/note_view.ex
+++ lib/mirage_web/views/note_view.ex
@@ -1,7 +1,3 @@
 defmodule MirageWeb.NoteView do
   use MirageWeb, :view
-
-  def datetime_for_display(datetime) do
-    Timex.format!(datetime, "{D}. {Mshort} {YYYY}, {h24}:{m}")
-  end
 end
