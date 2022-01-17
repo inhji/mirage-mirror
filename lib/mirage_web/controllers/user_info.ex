e697c0ec03a426b6367b257dbb546ad103040551
Inhji <johnnie@posteo.de>
2022-01-17 19:30:04 +0100

    feat: add indieauth/micropub info

--- lib/mirage_web/controllers/user_info.ex
+++ lib/mirage_web/controllers/user_info.ex
@@ -3,6 +3,9 @@ defmodule MirageWeb.UserInfo do
 
   alias Mirage.{Accounts, Identities}
 
+  @token_endpoint Application.compile_env!(:mirage, [:indie, :token_endpoint])
+  @auth_endpoint Application.compile_env!(:mirage, [:indie, :auth_endpoint])
+
   @doc """
   Assigns the identities of the user to the conn-object.
   Returns an empty list if no user is yet defined.
@@ -18,6 +21,14 @@ defmodule MirageWeb.UserInfo do
       end
 
     assign(conn, :identities, identities)
+  end
+
+  def fetch_indie_config(conn, _opts) do
+    assign(conn, :indie_config, %{
+      token_endpoint: @token_endpoint,
+      auth_endpoint: @auth_endpoint,
+      micropub_endpoint: "/indie/micropub"
+    })
   end
 
   def fetch_motd(conn, _opts) do
