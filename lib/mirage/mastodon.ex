9b11199b7d770ea1a651dcbb45e3e340ad6a68b2
Inhji <johnnie@posteo.de>
2022-01-22 00:25:19 +0100

    fix: loading of env vars

--- lib/mirage/mastodon.ex
+++ lib/mirage/mastodon.ex
@@ -1,16 +1,15 @@
 defmodule Mirage.Mastodon do
   use Tesla
 
-  @host Application.compile_env!(:mirage, [:mastodon, :instance_host])
-  @id Application.compile_env!(:mirage, [:mastodon, :client_id])
-  @secret Application.compile_env!(:mirage, [:mastodon, :client_secret])
   @redirect_uri "urn:ietf:wg:oauth:2.0:oob"
-  @scopes "read write"
+  @scope "read write"
 
   defp client() do
+    host = get_config(:instance_host)
+
     middleware = [
       Tesla.Middleware.JSON,
-      {Tesla.Middleware.BaseUrl, "https://#{@host}"},
+      {Tesla.Middleware.BaseUrl, "https://#{host}"},
       {Tesla.Middleware.Headers, [{"User-Agent", Mirage.user_agent()}]}
     ]
 
@@ -18,33 +17,44 @@ defmodule Mirage.Mastodon do
   end
 
   def authorize_url() do
+    host = get_config(:instance_host)
+    id = get_config(:client_id)
+
     %URI{
       scheme: "https",
-      host: @host,
+      host: host,
       path: "/oauth/authorize",
       query:
         URI.encode_query(%{
           response_type: "code",
-          client_id: @id,
+          client_id: id,
           redirect_uri: @redirect_uri,
-          scope: @scopes
+          scope: @scope
         })
     }
     |> URI.to_string()
   end
 
   def get_token(auth_code) do
+    id = get_config(:client_id)
+    secret = get_config(:client_secret)
+
     post(
       client(),
       "/oauth/token",
       %{
         grant_type: "authorization_code",
-        client_id: @id,
-        client_secret: @secret,
+        client_id: id,
+        client_secret: secret,
+        code: auth_code,
         redirect_uri: @redirect_uri,
-        scope: @scopes,
-        code: auth_code
+        scope: @scope
       }
     )
+  end
+
+  defp get_config(key) do
+    config = Application.get_env(:mirage, :mastodon)
+    Keyword.get(config, key)
   end
 end
