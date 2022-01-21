49250ce07e733e5d201bfc3ceddb136936a37347
Inhji <johnnie@posteo.de>
2022-01-22 00:10:56 +0100

    feat: add user_agent/0

--- lib/mirage.ex
+++ lib/mirage.ex
@@ -6,4 +6,8 @@ defmodule Mirage do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  @user_agent Application.compile_env!(:mirage, [MirageWeb.Endpoint, :user_agent])
+
+  def user_agent(), do: @user_agent
 end
