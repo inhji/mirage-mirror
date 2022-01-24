ce297ad0bb2d370c9d71771b6e4e220252962ff1
Inhji <johnnie@posteo.de>
2022-01-24 23:33:10 +0100

    feat: enable oauth logging in dev

--- config/dev.exs
+++ config/dev.exs
@@ -87,6 +87,9 @@ config :mirage, MirageWeb.Endpoint,
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
 
+# Enable OAuth logging
+config :oauth2, debug: true
+
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
 config :phoenix, :stacktrace_depth, 20
