47d04b06ca9b7e25c9ee2e88d202a58c07355dae
Inhji <johnnie@posteo.de>
2022-01-26 08:52:34 +0100

    feat: remove bookmarks from main feed

--- lib/mirage/content.ex
+++ lib/mirage/content.ex
@@ -1,10 +1,9 @@
 defmodule Mirage.Content do
   def list_updates() do
-    notes = Mirage.Notes.list_published_notes()
-    bookmarks = Mirage.Bookmarks.list_published_bookmarks()
-
-    Enum.sort(bookmarks ++ notes, fn item1, item2 ->
-      NaiveDateTime.compare(item1.published_at, item2.published_at) == :gt
-    end)
+    Mirage.Notes.list_published_notes()
+    # bookmarks = Mirage.Bookmarks.list_published_bookmarks()
+    # Enum.sort(bookmarks ++ notes, fn item1, item2 ->
+    #   NaiveDateTime.compare(item1.published_at, item2.published_at) == :gt
+    # end)
   end
 end
