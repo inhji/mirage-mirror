a161d9c681ee240e25a431849452d4bd9dfd88cc
Inhji <johnnie@posteo.de>
2022-01-30 19:27:43 +0100

    feat: create empty syndication entries in note hooks

--- lib/mirage/notes/note_hooks.ex
+++ lib/mirage/notes/note_hooks.ex
@@ -4,8 +4,10 @@ defmodule Mirage.Notes.NoteHooks do
 
   Right now the available hooks are:
 
+  * `publish/2`
   * `update_tags/2`
-  * `send_webmentions/2`
+  * `send_webmentions_if_published/2`
+  * `syndicate_to_if_published/2`
 
   This could be refactored in a more general purpose hooks module.
   """
@@ -13,13 +15,19 @@ defmodule Mirage.Notes.NoteHooks do
   require Logger
   alias MirageWeb.Router.Helpers, as: Routes
 
-  def run_hooks(result, attrs) do
-    Mirage.Hooks.run(result, attrs, [
-      &publish/2,
-      &update_tags/2,
-      &send_webmentions/2,
-      &syndicate_to/2
-    ])
+  def run_hooks(note, attrs) do
+    Mirage.Hooks.run(
+      note,
+      attrs,
+      [
+        &publish/2,
+        &update_tags/2,
+        &create_syndications/2,
+        &send_webmentions_if_published/2,
+        &syndicate_to_if_published/2
+      ],
+      &Mirage.Notes.get_note!(&1.slug)
+    )
   end
 
   def publish(note, attrs) do
@@ -37,15 +45,42 @@ defmodule Mirage.Notes.NoteHooks do
      |> Mirage.Tags.TagUpdater.update_tags(attrs)}
   end
 
-  def send_webmentions(note, _attrs) do
+  def send_webmentions_if_published(note, _attrs) do
     # Only syndicate if note is already published
-    if note.published_at do
+    if !!note.published_at do
       url = Routes.note_url(MirageWeb.Endpoint, :show, note)
       Mirage.Indie.WebmentionWorker.run(url)
     end
   end
 
-  def syndicate_to(note, attrs) do
+  def create_syndications(note, attrs) do
+    Logger.info("Syndication targets: '#{Enum.count(note.syndications)}'")
+
+    if Map.has_key?(attrs, "syndication_targets") and
+         not Enum.empty?(attrs["syndication_targets"]) do
+      Enum.map(attrs["syndication_targets"], fn target ->
+        case Mirage.NoteSyndications.get_syndication(note, target) do
+          nil ->
+            Logger.info("Creating new syndication for target '#{target}'")
+
+            {:ok, syndication} =
+              Mirage.NoteSyndications.create_syndication(%{
+                note_id: note.id,
+                type: String.to_existing_atom(target)
+              })
+
+            syndication
+
+          syndication ->
+            Logger.info("Syndication for '#{target}' already exists.")
+
+            syndication
+        end
+      end)
+    end
+  end
+
+  def syndicate_to_if_published(note, attrs) do
     # Only send webmentions if note is already published
     if !!note.published_at and Map.has_key?(attrs, "syndication_targets") do
       targets = attrs["syndication_targets"]
