6df7081967a3eed45838b2bfff8588f9c0fc96b0
Inhji <johnnie@posteo.de>
2022-01-30 19:23:18 +0100

    feat: add setup function for hooks

--- lib/mirage/hooks.ex
+++ lib/mirage/hooks.ex
@@ -7,17 +7,18 @@ defmodule Mirage.Hooks do
 
   require Logger
 
-  def run({:ok, schema} = result, attrs, hooks) do
+  def run({:ok, schema} = result, attrs, hooks, result_setup) do
     Enum.each(hooks, fn hook ->
       info = Function.info(hook)
       Logger.info("Running #{info[:name]} for #{schema.slug}")
+      schema = result_setup.(schema)
       hook.(schema, attrs)
     end)
 
     result
   end
 
-  def run(result, _attrs, _hooks) do
+  def run(result, _attrs, _hooks, _result_setup) do
     result
   end
 end
