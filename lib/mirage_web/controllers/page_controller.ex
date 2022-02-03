8db2316c453dbd5161754fa7ce37652d7609cd7a
Inhji <johnnie@posteo.de>
2022-02-03 23:43:54 +0100

    feat: only make public pages 45rem wide, clean up, fix missing hcard on home

--- lib/mirage_web/controllers/page_controller.ex
+++ lib/mirage_web/controllers/page_controller.ex
@@ -3,10 +3,12 @@ defmodule MirageWeb.PageController do
 
   def index(conn, params) do
     page = Mirage.Content.list_updates(params)
+    user = Mirage.Accounts.get_user()
 
     render(conn, "index.html",
       page_title: "Home",
       page: page,
+      user: user,
       updates: page.entries,
       page_number: page.page_number,
       page_size: page.page_size,
@@ -17,6 +19,7 @@ defmodule MirageWeb.PageController do
 
   def about(conn, _params) do
     user = Mirage.Accounts.get_user()
+
     render(conn, "about.html", page_title: "About", user: user)
   end
 end
