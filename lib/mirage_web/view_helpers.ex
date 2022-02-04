99ac39345da936d7cb9ae0f45ef19f0d536a3728
Inhji <johnnie@posteo.de>
2022-02-04 23:12:32 +0100

    feat: add datetime_from_now helper

--- lib/mirage_web/view_helpers.ex
+++ lib/mirage_web/view_helpers.ex
@@ -2,4 +2,7 @@ defmodule MirageWeb.ViewHelpers do
   def datetime_for_display(datetime) do
     Timex.format!(datetime, "{D}. {Mshort} {YYYY}, {h24}:{m}")
   end
+
+  def datetime_from_now(nil), do: nil
+  def datetime_from_now(datetime), do: Timex.from_now(datetime)
 end
