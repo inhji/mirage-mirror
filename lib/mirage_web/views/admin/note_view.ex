6e076ece1f0c645e7d665d454070040d971b3afb
Inhji <johnnie@posteo.de>
2022-02-05 11:24:03 +0100

    feat: show selected syndications in note form

--- lib/mirage_web/views/admin/note_view.ex
+++ lib/mirage_web/views/admin/note_view.ex
@@ -4,4 +4,8 @@ defmodule MirageWeb.Admin.NoteView do
   def tags_to_string(tags) when is_list(tags) do
     Enum.map_join(tags, ", ", fn tag -> tag.title end)
   end
+
+  def targets_to_list(targets) do
+    Enum.map(targets, fn target -> target.type end)
+  end
 end
