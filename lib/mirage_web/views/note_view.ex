f57bde2e1b23d227b27247483da5926e9be65fe7
Inhji <johnnie@posteo.de>
2022-02-06 13:58:21 +0100

    feat: add openwebicons, show syndications

--- lib/mirage_web/views/note_view.ex
+++ lib/mirage_web/views/note_view.ex
@@ -1,6 +1,12 @@
 defmodule MirageWeb.NoteView do
   use MirageWeb, :view
 
-  def syndication_url(%{url: nil} = syndication), do: "#"
-  def syndication_url(%{url: url} = syndication), do: url
+  def syndication_url(%{url: nil}), do: "#"
+  def syndication_url(%{url: url}), do: url
+
+  def syndication_icon(%{type: :mastodon}), do: "openwebicons-mastodon-simple"
+  def syndication_icon(%{type: _}), do: ""
+
+  def syndication_text(%{type: :mastodon}), do: "Mastodon"
+  def syndication_text(%{type: _}), do: ""
 end
