29a97ca23eefd409678600da6e3d144a875a5531
Inhji <johnnie@posteo.de>
2022-02-10 08:46:47 +0100

    feat: dont show content for likes

--- lib/mirage_web/views/note_view.ex
+++ lib/mirage_web/views/note_view.ex
@@ -1,5 +1,6 @@
 defmodule MirageWeb.NoteView do
   use MirageWeb, :view
+  alias Mirage.Notes.Note
 
   def syndication_url(%{url: nil}), do: "#"
   def syndication_url(%{url: url}), do: url
@@ -9,4 +10,15 @@ defmodule MirageWeb.NoteView do
 
   def syndication_text(%{type: :mastodon}), do: "Mastodon"
   def syndication_text(%{type: _}), do: ""
+
+  def bookmark?(%Note{url: url, url_type: "bookmark_of"}) when is_binary(url), do: true
+  def bookmark?(_), do: false
+
+  def like?(%Note{url: url, url_type: "like_of"}) when is_binary(url), do: true
+  def like?(_), do: false
+
+  def reply?(%Note{url: url, url_type: "reply_of"}) when is_binary(url), do: true
+  def reply?(_), do: false
+
+  @dialyzer {:no_return, {:template_not_found, 2}}
 end
