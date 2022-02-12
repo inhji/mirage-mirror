7d953909054827e81aad8804c7ae7a0c16f1e90a
Inhji <johnnie@posteo.de>
2022-02-12 12:21:57 +0100

    feat: add page, like partials for index page

--- lib/mirage_web/views/page_view.ex
+++ lib/mirage_web/views/page_view.ex
@@ -1,4 +1,19 @@
 defmodule MirageWeb.PageView do
   use MirageWeb, :view
   import Scrivener.HTML
+
+  def render_update(%Mirage.Notes.Note{} = update, assigns) do
+    new_assigns = Map.put(assigns, :note, update)
+
+    cond do
+      like?(update) ->
+        render(MirageWeb.NoteView, "like.html", new_assigns)
+
+      page?(update) ->
+        render(MirageWeb.NoteView, "page.html", new_assigns)
+
+      true ->
+        render(MirageWeb.NoteView, "note.html", new_assigns)
+    end
+  end
 end
