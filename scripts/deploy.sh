2ccc5270ecb477a5d6cb9e7f4cae808ab01fae81
Inhji <johnnie@posteo.de>
2022-02-15 23:03:06 +0100

    fix: improve build script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,14 +6,10 @@ echo "--------------------------"
 rsync -avz . mirage@inhji.de:/opt/mirage2 --progress --delete --exclude-from .rsyncignore
 
 echo ""
-echo "Build starting!"
+echo "Building and restarting!"
 echo "--------------------------"
-ssh -T mirage@glados << EOSSH
+ssh -T mirage@inhji.de << EOSSH
 cd /opt/mirage2
 ./scripts/build.sh
+sudo systemctl restart mirage2
 EOSSH
-
-echo ""
-echo "Build complete, restarting..."
-echo "--------------------------"
-ssh -T mirage@glados sudo systemctl restart mirage2
