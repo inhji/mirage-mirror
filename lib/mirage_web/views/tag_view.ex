37ddc41a764cb3b6851d2f4e8e347c8573fcf2ed
Inhji <johnnie@posteo.de>
2022-02-15 08:55:35 +0100

    feat: show unpublished notes in a tag separately, dont show empty tags to logged out users

--- lib/mirage_web/views/tag_view.ex
+++ lib/mirage_web/views/tag_view.ex
@@ -1,3 +1,15 @@
 defmodule MirageWeb.TagView do
   use MirageWeb, :view
+
+  def note_count(conn, %{notes: notes, notes_unpublished: notes_unpublished} = _tag) do
+    if conn.assigns.current_user do
+      Enum.count(notes ++ notes_unpublished)
+    else
+      Enum.count(notes)
+    end
+  end
+
+  def has_unpublished_notes?(conn, tag) do
+    !!conn.assigns.current_user and not Enum.empty?(tag.notes_unpublished)
+  end
 end
