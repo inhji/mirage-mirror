26425b914f746f4bf00931dfce1c3ff59167d809
Inhji <johnnie@posteo.de>
2022-02-16 20:54:27 +0100

    fix: revert build script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,10 +6,14 @@ echo "--------------------------"
 rsync -avz . mirage@inhji.de:/opt/mirage2 --progress --delete --exclude-from .rsyncignore
 
 echo ""
-echo "Building and restarting!"
+echo "Build starting!"
 echo "--------------------------"
 ssh -T mirage@inhji.de << EOSSH
 cd /opt/mirage2
 ./scripts/build.sh
-sudo systemctl restart mirage2
 EOSSH
+
+echo ""
+echo "Build complete, restarting..."
+echo "--------------------------"
+ssh -T mirage@inhji.de sudo systemctl restart mirage2
