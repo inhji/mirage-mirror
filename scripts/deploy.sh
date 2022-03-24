fe8b1b51e589091ad60e7d0f6916373ff0a28c98
Inhji <johnnie@posteo.de>
2022-03-24 19:32:36 +0100

    feat: use env vars in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
+MIRAGE_HOST=inhji.de
+MIRAGE_DIR=/home/inhji/www/mirage/app
+
 echo ""
 echo "Pushing changes..."
 echo "--------------------------"
-rsync -avz . mirage@inhji.de:/opt/mirage2 --progress --delete --exclude-from .rsyncignore
+rsync -avz . $MIRAGE_HOST:$MIRAGE_DIR --progress --delete --exclude-from .rsyncignore
 
 echo ""
 echo "Build starting!"
 echo "--------------------------"
-ssh -T mirage@inhji.de << EOSSH
-cd /opt/mirage2
+ssh -T $MIRAGE_HOST << EOSSH
+cd $MIRAGE_DIR
 ./scripts/build.sh
 EOSSH
 
 echo ""
 echo "Build complete, restarting..."
 echo "--------------------------"
-ssh -T mirage@inhji.de sudo systemctl restart mirage2
+ssh -T $MIRAGE_HOST sudo systemctl restart mirage
