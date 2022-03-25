ec308b6aaf0e89d1c1e802e8c0d5649a672e7179
Inhji <johnnie@posteo.de>
2022-03-25 07:26:20 +0100

    feat: new theme selector

--- assets/js/dark.js
+++ assets/js/dark.js
@@ -1,19 +1,16 @@
 export default function darkMode() {
-	const darkModeToggle = document.querySelector(".theme-toggle")
+	const darkModeToggles = document.querySelectorAll(".theme-toggle")
+	console.log(darkModeToggles)
 
-	darkModeToggle.addEventListener('click' ,function (e) {
-		e.preventDefault()
+	darkModeToggles.forEach(toggle => {
+		console.log("Toggle")
+		toggle.addEventListener('click', e => {
+			e.preventDefault()
 
-		if (document.documentElement.dataset.theme === "day") {
-			//document.body.classList.remove("dark")
-			document.documentElement.dataset.theme = "night"
-			localStorage.theme = 'night'
-			console.log("Night Theme loaded!")
-		} else {
-			// document.body.classList.add("dark")
-			document.documentElement.dataset.theme = "day"
-			localStorage.theme = 'day'
-			console.log("Day Theme loaded!")
-		}
+			const theme = toggle.dataset.theme
+			document.documentElement.dataset.theme = theme
+			localStorage.theme = theme
+			console.log(`Theme '${theme}' loaded!`)
+		})
 	})
 }
